5eb0244e14ff2f7983f3748f9f0478eebbecfd35
Adam Khalifa <akhalifa1982@gmail.com>
2022-08-11 17:30:14 +0300

    initiate project

---
+++ Bull'sEye game/Bull'sEye game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bull'sEye game
+//
+//  Created by Adam Khalifa on 11.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
